604afebdbe23cbb2ebae9af170289bd88de11a3c
Stephen Tozer <stephen.tozer@sony.com>
2024-08-14 15:13:11 +0100

    Add File Args table to OriginalDIDebugify report

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -904,12 +904,26 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
   DebugifyEachInstrumentation Debugify;
   DebugInfoPerPass DebugInfoBeforePass;
   if (CodeGenOpts.EnableDIPreservationVerify) {
+
     Debugify.setDebugifyMode(DebugifyMode::OriginalDebugInfo);
     Debugify.setDebugInfoBeforePass(DebugInfoBeforePass);
 
-    if (!CodeGenOpts.DIBugsReportFilePath.empty())
+    if (!CodeGenOpts.DIBugsReportFilePath.empty()) {
       Debugify.setOrigDIVerifyBugsReportFilePath(
           CodeGenOpts.DIBugsReportFilePath);
+      std::error_code EC;
+      raw_fd_ostream OS_FILE{CodeGenOpts.DIBugsReportFilePath, EC,
+                    sys::fs::OF_Append | sys::fs::OF_TextWithCRLF};
+      if (EC) {
+        errs() << "Could not open file: " << EC.message() << ", "
+              << CodeGenOpts.DIBugsReportFilePath << '\n';
+      } else {
+        if (auto L = OS_FILE.lock()) {
+          OS_FILE << CodeGenOpts.DIBugsReportArgString;
+        }
+        OS_FILE.close();
+      }
+    }
     Debugify.registerCallbacks(PIC, MAM);
 
 #ifdef LLVM_ENABLE_DEBUGLOC_COVERAGE_TRACKING
