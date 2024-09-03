c9862be13ee82203d2b7ee6b56d9778614f93bdb
Stephen Tozer <Stephen.Tozer@sony.com>
2024-09-03 17:01:39 +0000

    Reapply some missing work

--- llvm/lib/IR/IRBuilder.cpp
+++ llvm/lib/IR/IRBuilder.cpp
@@ -65,15 +65,10 @@ DebugLoc IRBuilderBase::getCurrentDebugLocation() const {
   return StoredDL;
 }
 void IRBuilderBase::SetInstDebugLocation(Instruction *I) const {
-  for (const auto &KV : MetadataToCopy)
-    if (KV.first == LLVMContext::MD_dbg) {
-      I->setDebugLoc(StoredDL.getCopied());
-      return;
-    }
   // If I does not have an existing DebugLoc and no DebugLoc has been set
   // here, we copy our DebugLoc to I anyway, because more likely than not I
   // is a new instruction whose DL should originate from this builder.
-  if (!I->getDebugLoc())
+  if (HasExplicitDL || !I->getDebugLoc())
     I->setDebugLoc(StoredDL.getCopied());
 }
 
