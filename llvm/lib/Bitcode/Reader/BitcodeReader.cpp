ec995a25e5d0a1cbc12a21a95d4f31b85f9bb926
Stephen Tozer <stephen.tozer@sony.com>
2026-05-22 14:10:25 +0100

    Undo some more mass replace

--- llvm/lib/Bitcode/Reader/BitcodeReader.cpp
+++ llvm/lib/Bitcode/Reader/BitcodeReader.cpp
@@ -6731,7 +6731,7 @@ Error BitcodeReader::parseFunctionBody(Function *F) {
       Instruction *Inst = getLastInstruction();
       if (!Inst)
         return error("Invalid dbg record: missing instruction");
-      DebugLoc DIL = cast<DILocation>(getFnMetadataByID(Record[0]));
+      DILocation *DIL = cast<DILocation>(getFnMetadataByID(Record[0]));
       DILabel *Label = cast<DILabel>(getFnMetadataByID(Record[1]));
       Inst->getParent()->insertDbgRecordBefore(
           new DbgLabelRecord(Label, DebugLoc(DIL)), Inst->getIterator());
@@ -6763,7 +6763,7 @@ Error BitcodeReader::parseFunctionBody(Function *F) {
       //   ..., LocationMetadata, DIAssignID, DIExpression, LocationMetadata
       unsigned Slot = 0;
       // Common fields (0-2).
-      DebugLoc DIL = cast<DILocation>(getFnMetadataByID(Record[Slot++]));
+      DILocation *DIL = cast<DILocation>(getFnMetadataByID(Record[Slot++]));
       DILocalVariable *Var =
           cast<DILocalVariable>(getFnMetadataByID(Record[Slot++]));
       DIExpression *Expr =
