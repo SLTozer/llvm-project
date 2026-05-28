ec281eb7ccbffa62c6ae7fa99a93503c1b97d8f0
Stephen Tozer <stephen.tozer@sony.com>
2026-05-28 10:41:28 +0100

    Remove unused function

--- llvm/lib/IR/DebugInfo.cpp
+++ llvm/lib/IR/DebugInfo.cpp
@@ -481,38 +481,6 @@ bool DebugInfoFinder::addMacro(DIMacro *Macro, DIMacroFile *MacroFile) {
   return true;
 }
 
-/// Recursively handle DILocations in followup metadata etc.
-///
-/// TODO: If for example a followup loop metadata would reference itself this
-/// function would go into infinite recursion. We do not expect such cycles in
-/// the loop metadata (except for the self-referencing first element
-/// "LoopID"). However, we could at least handle such situations more gracefully
-/// somehow (e.g. by keeping track of visited nodes and dropping metadata).
-static Metadata *updateLoopMetadataDebugLocationsRecursive(
-    Metadata *MetadataIn, function_ref<Metadata *(Metadata *)> Updater) {
-  const MDTuple *M = dyn_cast_or_null<MDTuple>(MetadataIn);
-  // The loop metadata options should start with a MDString.
-  if (!M || M->getNumOperands() < 1 || !isa<MDString>(M->getOperand(0)))
-    return MetadataIn;
-
-  bool Updated = false;
-  SmallVector<Metadata *, 4> MDs{M->getOperand(0)};
-  for (Metadata *MD : llvm::drop_begin(M->operands())) {
-    if (!MD) {
-      MDs.push_back(nullptr);
-      continue;
-    }
-    Metadata *NewMD =
-        Updater(updateLoopMetadataDebugLocationsRecursive(MD, Updater));
-    if (NewMD)
-      MDs.push_back(NewMD);
-    Updated |= NewMD != MD;
-  }
-
-  assert(!M->isDistinct() && "M should not be distinct.");
-  return Updated ? MDNode::get(M->getContext(), MDs) : MetadataIn;
-}
-
 static MDNode *updateLoopMetadataDebugLocationsImpl(
     MDNode *OrigLoopID, function_ref<Metadata *(Metadata *)> Updater) {
   assert(OrigLoopID && OrigLoopID->getNumOperands() > 0 &&
